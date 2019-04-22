57614da1d6260bdb5801da9b4ca9c6e703dd0dd8
Товпинец Кирилл <C:\Users\Tovpinec\AppData\Roaming\The Bat!>
2019-04-22 12:55:16 +0300

    feat: add gothampro font

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
 							{
 								loader: 'file-loader',
 								options: {
-									name: '[hash].[ext]',
+									name: '[name].[ext]',
 									outputPath: 'styles'
 								},
 							}
